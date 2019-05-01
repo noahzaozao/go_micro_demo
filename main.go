e0f0fd35c8b8b5638399d925a2ae70c087dfbda0
noahwang <234082230@qq.com>
2019-05-01 22:22:14 +0800

    update

--- main.go
+++ main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"log"
-	"github.com/micro/go-micro"
+	"time"
+	"context"
 
-	//_ "github.com/micro/go-plugins/broker/nsq"
-	_ "github.com/micro/go-plugins/registry/etcd"
+	"go.etcd.io/etcd/client"
 )
 
-
 func main() {
-	service := micro.NewService(
-		micro.Name("go.micro.demo"),
-		micro.Version("latest"),
-	)
-	service.Init()
-
-	// Run service
-	if err := service.Run(); err != nil {
+	cfg := client.Config{
+		Endpoints:               []string{"http://127.0.0.1:2379"},
+		Transport:               client.DefaultTransport,
+		// set timeout per request to fail fast when the target endpoint is unavailable
+		HeaderTimeoutPerRequest: time.Second,
+	}
+	c, err := client.New(cfg)
+	if err != nil {
 		log.Fatal(err)
+	}
+	kapi := client.NewKeysAPI(c)
+	// set "/foo" key with "bar" value
+	log.Print("Setting '/foo' key with 'bar' value")
+	resp, err := kapi.Set(context.Background(), "/foo", "bar", nil)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		// print common key info
+		log.Printf("Set is done. Metadata is %q\n", resp)
+	}
+	// get "/foo" key's value
+	log.Print("Getting '/foo' key value")
+	resp, err = kapi.Get(context.Background(), "/foo", nil)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		// print common key info
+		log.Printf("Get is done. Metadata is %q\n", resp)
+		// print value
+		log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
 	}
 }
